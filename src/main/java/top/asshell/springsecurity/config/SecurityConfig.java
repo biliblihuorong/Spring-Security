63500f6e587e5009e6cd0ef7d740c927cfb30051
Misakashell <Misakashell@duck.com>
2022-10-27 20:41:13 +0800

    v0.0.0.3 单个权限设置

--- src/main/java/top/asshell/springsecurity/config/SecurityConfig.java
+++ src/main/java/top/asshell/springsecurity/config/SecurityConfig.java
@@ -34,9 +34,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http.formLogin()
                 .loginPage("/login.html") //设置自己的登录页面
                 .loginProcessingUrl("/user/login") //设置登录访问的Controller
-                .defaultSuccessUrl("/user/index").permitAll()//设置登录成功后，跳转路径
+                .defaultSuccessUrl("/user/index")
+                .permitAll()//设置登录成功后，跳转路径
                 .and().authorizeRequests()
                 .antMatchers("/","/test/hello","/user/login").permitAll()//访问这些路径的时候不需要认证
+                .antMatchers("/test/index").hasAnyAuthority("admin")//设置权限访问
                 .anyRequest().authenticated()
                 .and().csrf().disable();
 
